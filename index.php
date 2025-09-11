1700acef81c94d21eb284d4d3d670d41e76323f8
Cody Mac Vay <cody.macvay1@gmail.com>
2025-09-11 09:21:39 +0100

    Commented all my code

--- index.php
+++ index.php
@@ -1,26 +1,27 @@
 <?php  // Starts the php
-echo "  // Use echo to output the entire HTML page
-<!DOCTYPE html>  // Declares the document as HTML
+// Uses echo to run the html
+echo " 
+<!DOCTYPE html>  <!--Declares the document as HTML --> 
 <html>
 <head>
     <title>Home</title>
-    <link rel='stylesheet' href='CSS/style.css'>  // Connects external css for styling
+    <link rel='stylesheet' href='CSS/style.css'>  <!-- Connects external css for styling -->
 </head>
 <body>
     <header>
-        <img src='images/home.jpg' alt='Home Banner'>  // Image to display in the header of the home page.
+        <img src='images/home.jpg' alt='Home Banner'>  <!--Image to display in the header of the home page. -->
     </header>
     <nav>
-        <a href='index.php'>Home</a>  // Link to home page
-        <a href='about.php'>About</a>  // Link to about page.
-        <a href='contact.php'>Contact</a>  // Link to contact page.
-        <a href='signup.php'>Signup</a>  // Link to signup page.
+        <a href='index.php'>Home</a>  <!-- Link to Home page. -->
+        <a href='about.php'>About</a> <!-- Link to About page. -->
+        <a href='contact.php'>Contact</a> <!-- Link to Contact page. -->
+        <a href='signup.php'>Signup</a> <!-- Link to signup page. -->
     </nav>
 
-// Main section
+<!-- Main Content -->
     <div class='section'>
         <h1>Welcome To My Website</h1>
-        // Features section
+        <!-- Features section -->
         <div class='features'>
             <div class='feature'>
                 <img src='images/test.jpg' alt='Testing my knowledge'>
@@ -38,11 +39,11 @@ echo "  // Use echo to output the entire HTML page
                 <p>We try to use CSS to the best of our knowledge:)</p>
             </div>
         </div>
-        // Call to action section
+        <!-- Call to action section -->
         <div class='cta'>
-            <p>Create your account now</p>  // Motivation text
-            <form action='signup.php'>  // Redirects you to the signup.php page
-                <button type='submit'>Sign Up Now</button>  // Creates a button to use to redirect you to that signup.php page
+            <p>Create your account now</p>  <!-- Motivation text to signup --> 
+            <form action='signup.php'>  <!-- Redirects you to the signup.php page --> 
+                <button type='submit'>Sign Up Now</button> <!-- Creates a button to use to redirect you to that signup.php page -->
             </form>
         </div>
     </div>
